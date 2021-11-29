e6b1b7a922756bd5eaa8772f1b7d697ece4784ce
Volv0zachto <91129681+Volv0zachto@users.noreply.github.com>
2021-11-30 01:11:49 +0300

    Задание к теме 12
    Задание к теме 12

---
+++ Задание к теме 12/2.cpp
@@ -0,0 +1,89 @@
+﻿// 2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+#include <string>
+#include<math.h>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int A, B, X;
+	setlocale(LC_ALL, "Rus");
+	string C;
+	cout << "Введите символ C = ";
+	cin >> C;
+
+	cout << "Введите команду X = ";
+	cin >> X;
+	if (X > -2 and X < 2)
+	{
+		if (C == "С")
+		{
+			switch (X)
+			{
+			case -1:
+				cout << "Восток";
+				break;
+			case 0:
+				cout << "Север";
+				break;
+			case 1:
+				cout << "Запад";
+				break;
+			}
+		}
+		if (C == "З")
+		{
+			switch (X)
+			{
+			case -1:
+				cout << "Север";
+				break;
+			case 0:
+				cout << "Запад";
+				break;
+			case 1:
+				cout << "Юг";
+				break;
+			}
+		}
+		if (C == "Ю")
+		{
+			switch (X)
+			{
+			case -1:
+				cout << "Запад";
+				break;
+			case 0:
+				cout << "Юг";
+				break;
+			case 1:
+				cout << "Восток";
+				break;
+			}
+		}
+		if (C == "В")
+		{
+			switch (X)
+			{
+			case -1:
+				cout << "Юг";
+				break;
+			case 0:
+				cout << "Восток";
+				break;
+			case 1:
+				cout << "Север";
+				break;
+			}
+		}
+	}
+	
+	return 0;
+}
+
